8f99c40ea19d4f7485c92d91b6c1a0c36a8ba734
watt <jack.a.watt@gmail.com>
2017-05-15 16:28:00 -0300

    initial commit

---
+++ pomodoro.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+COUNTER=0
+while [  $COUNTER -lt 10 ]; do
+  let COUNTER=COUNTER+1 
+  say "main start"
+  clear
+  cat task.txt
+  sleep 1500 
+  clear
+  if [ $COUNTER -eq "4" ]; then
+    cat long.txt
+    say "long"
+  fi
+  say "break start"
+  cat break.txt
+  sleep 300
+  say "break done"
+  if [ $COUNTER -eq "4" ]; then
+    let COUNTER=0
+    sleep 900
+  fi
+
+done
